6c693f182794686f6842f71b3bd31e0da7f6a26b
irna fitriani <irna.fitriani@iconpln.co.id>
2018-12-09 18:36:31 +0700

    Setup Project

---
+++ MovieOMDB/MovieOMDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieOMDB
+//
+//  Created by irna on 12/9/18.
+//  Copyright © 2018 irna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MovieOMDB/Pods/Target Support Files/Pods-MovieOMDBTests/Pods-MovieOMDBTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieOMDBTests : NSObject
+@end
+@implementation PodsDummy_Pods_MovieOMDBTests
+@end
